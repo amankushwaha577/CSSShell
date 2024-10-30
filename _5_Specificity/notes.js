f5868b7c57a192bc12aec21ad836d282eb2b1ebe
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-10-30 18:13:09 +0530

    specificty

---
+++ _5_Specificity/notes.js
@@ -0,0 +1,15 @@
+
+
+// CSS Priority Orders :- 
+__________________________________________________________________
+// +--------------------------------+-----------------------------+
+// | CSS Priority Order             | Description                 |
+// +--------------------------------+-----------------------------+
+// | 1. Inline styles (highest)     | style="color: red;"         |
+// | 2. Internal styles             | Written in <style> tag      |
+// | 3. External styles             | Linked via <link> tag       |
+// | 4. Browser default styles      | Lowest priority             |
+// +--------------------------------+-----------------------------+
+
+
+
