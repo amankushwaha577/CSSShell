e9e0f5f2d6089f98a398efeacfe36128f2a41c80
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-02-19 07:47:21 +0530

    specificity

--- _105_Specificity/notes.js
+++ _105_Specificity/notes.js
@@ -3,20 +3,11 @@ __________________________________________________________________
 // +--------------------------------+-----------------------------+
 // | CSS Priority Order             | Description                 |
 // +--------------------------------+-----------------------------+
-// | 1. Inline styles (highest)     | style="color: red;"         |
-// | 2. Internal styles             | Written in <style> tag      |
-// | 3. External styles             | Linked via <link> tag       |
-// | 4. Browser default styles      | Lowest priority             |
+// | 1. Inline styles (highest)     | style="color: red;"         |  Highest : Specificity: (1, 0, 0, 0)
+// | 2. Internal styles             | Written in <style> tag      |  Specificity depends on the type of selectors (IDs, classes, elements).
+// | 3. External styles             | Linked via <link> tag       |  Specificity depends on the type of selectors (IDs, classes, elements).
+// | 4. Browser default styles      | Lowest priority             |  Specificity depends on the type of selectors (IDs, classes, elements).
 // +--------------------------------+-----------------------------+
-
-
-
-// Priority of External, Internal, and Inline CSS:-
-__________________________________________________________________
-// Inline CSS: Has the highest priority (style attribute directly on the element). Specificity: (1, 0, 0, 0)
-// Internal CSS: Styles written within the <style> tag in the HTML <head> section. Specificity depends on the type of selectors (IDs, classes, elements).
-// External CSS: Styles written in an external file and linked to the HTML document via a <link> tag. Specificity also depends on the selectors(IDs, classes, elements), but it has lower priority compared to inline and internal styles.
-
 
 __________________________________________________________________
 // First Priority of CSS Is taken than specificity
